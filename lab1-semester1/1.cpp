0b4be5f52bf1009ef9db1f2cd044ea0d2d86bada
Azizullo <63806547+ahmymas@users.noreply.github.com>
2021-04-17 01:23:00 +0500

    Create 1.cpp
    Initial commit

---
+++ lab1-semester1/1.cpp
@@ -0,0 +1,12 @@
+//1.	Print “Hello Word”.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World"<<endl;
+    //this prints "Hello World" 
+    
+    system("pause");
+    return 0;
+}
