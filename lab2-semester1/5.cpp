1d2348502a0f6f3bb97e45edd4f01a3165e00db7
Azizullo Temirov <63806547+ahmymas@users.noreply.github.com>
2021-04-18 15:26:34 +0500

    Area of Rectangle

---
+++ lab2-semester1/5.cpp
@@ -0,0 +1,21 @@
+//C++  Program to Calculate Area of Rectangle
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float w, l, area;
+    cout<<"Input the width of rectangle. a = ";
+    cin>>w;
+    cout<<"Input the length of rectangle. b = ";
+    cin>>l;
+    
+    area=w*l;
+    
+    cout<<"Area of Rectangle = "<<area<<endl;
+
+    system("pause");
+    return 0;
+}
