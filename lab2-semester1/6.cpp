0ad5bd17dd41547aeabe6d96a5e27f48c117df7c
Azizullo Temirov <63806547+ahmymas@users.noreply.github.com>
2021-04-18 15:29:25 +0500

    Area of Square

---
+++ lab2-semester1/6.cpp
@@ -0,0 +1,19 @@
+//C++  Program to Calculate Area of Square
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, area;
+    cout<<"Input the side of Square: ";
+    cin>>a;
+    
+    area=a*a;
+    
+    cout<<"Area of Square = "<<area<<endl;
+
+    system("pause");
+    return 0;
+}
